4fc4ef44688834111663ab303721ddffe03c6401
j_tw_josh <j_tw_josh@tg.com>
2021-10-28 13:50:56 +0800

    Creating first Vue component : ToDoItem

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,4 +2,22 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <div id="app">
+    <h1>To-Do List</h1>
+    <ul>
+      <li>
+        <to-do-item label="My ToDo Item"></to-do-item>
+      </li>
+    </ul>
+  </div>
 </template>
+<script>
+import ToDoItem from '../components/ToDoItem.vue';
+
+export default {
+  name: 'app',
+  components: {
+    ToDoItem
+  }
+};
+</script>
