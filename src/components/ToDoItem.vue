ce133b91838858ec08b75a23e5bcbc3c70395d0f
j_tw_josh <j_tw_josh@tg.com>
2021-10-28 14:28:01 +0800

    use lodash.uniqueid to add toDoItem unique id

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
-    <input type="checkbox" id="todo-item"/>
-    <label for="todo-item">{{label}}</label>
+    <input type="checkbox" :id="id" :checked="isDone" />
+    <label :for="id">{{label}}</label>
   </div>
 </template>
 
 <script>
+
+import uniqueId from 'lodash.uniqueid';
 export default {
   name: "ToDoItem",
   props: {
     label: { required: true, type: String },
     done: { default: false, type: Boolean }
+  },
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  data():{} {
+    return {
+      isDone: this.done,
+      id: uniqueId('todo-')
+    };
   }
 }
 </script>
