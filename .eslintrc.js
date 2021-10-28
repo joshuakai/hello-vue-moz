d58a7e0194ed69dd8d81a39c0fb94ee6b846f17b
j_tw_josh <j_tw_josh@tg.com>
2021-10-28 14:49:14 +0800

    eslint : disable .vue return type check

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,10 +9,23 @@ module.exports = {
     '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    parser: {
+      'vue': 'espree'
+    }
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // disable the rule for all files
+    // "@typescript-eslint/explicit-module-boundary-types": "off"
+  },
+  'overrides': [
+    {
+      'files': ['*.vue'],
+      'rules': {
+        '@typescript-eslint/explicit-module-boundary-types': 'off'
+      }
+    }
+  ]
 }
