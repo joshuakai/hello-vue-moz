8d51ecc745fb11e08bbce10a34bc17a65174f5b3
j_tw_josh <j_tw_josh@tg.com>
2021-10-28 15:11:42 +0800

    #content : add toDoForm
    fix id lost

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,28 +4,32 @@
   </div>
   <div id="app">
     <h1>To-Do List</h1>
+    <to-do-form></to-do-form>
     <ul>
       <li v-for="item in ToDoItems" :key="item.id">
-        <to-do-item :label="item.label" :done="item.done"></to-do-item>
+        <to-do-item :id="item.id" :label="item.label" :done="item.done"></to-do-item>
       </li>
     </ul>
   </div>
 </template>
 <script>
+import uniqueId from 'lodash.uniqueid'
 import ToDoItem from '../components/ToDoItem.vue';
+import ToDoForm from '../components/ToDoForm.vue';
 
 export default {
   name: 'app',
   components: {
-    ToDoItem
+    ToDoItem,
+    ToDoForm
   },
   data() {
     return {
       ToDoItems: [
-        {label: 'Learn Vue', done: false},
-        {label: 'Create a Vue project with the CLI', done: true},
-        {label: 'Have fun', done: true},
-        {label: 'Create a to-do list', done: false}
+        { id: uniqueId('todo-'), label: 'Learn Vue', done: false },
+        { id: uniqueId('todo-'), label: 'Create a Vue project with the CLI', done: true },
+        { id: uniqueId('todo-'), label: 'Have fun', done: true },
+        { id: uniqueId('todo-'), label: 'Create a to-do list', done: false }
       ]
     };
   }
